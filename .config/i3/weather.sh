64e517a19c2541f39118ef1614adf387ac35a096
georgeabr <George>
2019-02-25 16:46:34 +0000

    first commit

--- .config/i3/weather.sh
+++ .config/i3/weather.sh
@@ -11,4 +11,5 @@ do
 done)
 
 # add the line below in your crontab, without the #
+# edit the crontab with crontab -e
 # */5 * * * * curl -s wttr.in/newcastle-upon-tyne?T | head -n 7 > ~/.weather.cache
