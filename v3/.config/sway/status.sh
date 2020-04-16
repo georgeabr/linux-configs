7ede7a969344150a33d12f074e3ae410ef8ab33f
George <george.abraham.ro@gmail.com>
2020-04-16 22:09:43 +0100

    modified

--- v3/.config/sway/status.sh
+++ v3/.config/sway/status.sh
@@ -85,7 +85,7 @@ function print_status {
   # use awk and cut to get keyboard layout
   # keyboard_layout=$(swaymsg -t get_inputs | jq '.[0].xkb_active_layout_name'|cut -c2-3|awk '{ print toupper($0) }')
   # use only awk to get the keyboard layout
-  keyboard_layout=$(swaymsg -t get_inputs | jq '.[1].xkb_active_layout_name'|awk '{ print toupper(substr($1,2,2)) }')
+  keyboard_layout=$(swaymsg -t get_inputs | jq '.[0].xkb_active_layout_name'|awk '{ print toupper(substr($1,2,2)) }')
   
 #  echo "🖧 $network_info $audio_info 🔋 $battery_info $time_of_day_symbol $date_formatted"
   echo " $mem_free| $cpu_load | / - $disk_free | 🖧 $network_info| $audio_info | $keyboard_layout | $battery_info | $date_formatted "
