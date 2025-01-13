7f70b6bd35696b830d895dce19c34c13c7e6f884
George Abraham <37941352+georgeabr@users.noreply.github.com>
2025-01-13 20:20:40 +0000

    Add files via upload

--- v5/.config/wezterm/wezterm.lua
+++ v5/.config/wezterm/wezterm.lua
@@ -1,1 +1,40 @@
+local wezterm = require 'wezterm'
 
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+-- a comment here
+
+return {
+	check_for_updates = false,
+	hide_tab_bar_if_only_one_tab = true,
+--	enable_wayland = false,
+	color_scheme = 'Builtin Dark',
+	default_cursor_style = 'SteadyUnderline',
+	window_decorations = "NONE",
+	font = wezterm.font 'IBM Plex Mono', font_size = 13,
+
+
+    keys = {
+        { key = '"', mods = "CTRL|SHIFT", action=wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }},
+        { key = '%', mods = "CTRL|SHIFT",action=wezterm.action.SplitHorizontal{args={'bash'}}},
+    },
+
+	window_frame = {
+		inactive_titlebar_bg = '#353535',
+    		active_titlebar_bg = '#000000',
+    		inactive_titlebar_fg = '#cccccc',
+    		active_titlebar_fg = '#d9d9d9',
+    		inactive_titlebar_border_bottom = '#2b2042',
+    		active_titlebar_border_bottom = '#2b2042',
+    		button_fg = '#cccccc',
+    		button_bg = '#2b2042',
+    		button_hover_fg = '#ffffff',
+    		button_hover_bg = '#3b3052',
+  	},
+
+}
